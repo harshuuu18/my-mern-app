ae566a8c85a6fddc58d4cd3b2e9c7cc3ae32df6f
harsh <probookh118@gmail.com>
2021-04-04 10:25:46 +0530

    lskjlfdj

--- server.js
+++ server.js
@@ -28,12 +28,11 @@ app.use(express.json());
 app.use(require('./routes/auth'));
 
 app.use(require('./routes/post'));
+app.use(express.static('client/build'));
 
 
 app.get("/", (req, res) =>{
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-
-    
 })
 
 if(process.env.NODE_ENV === 'production') {
