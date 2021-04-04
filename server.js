eee2b559df9d1e3c1e17734823321b73dbe7922d
harsh <probookh118@gmail.com>
2021-04-04 10:48:43 +0530

    firs

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ app.use(express.json());
 app.use(require('./routes/auth'));
 
 app.use(require('./routes/post'));
-app.use(express.static('client/build'));
 
 
 app.get("/", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.send("Hello From The Harsh")
+    
 })
 
 if(process.env.NODE_ENV === 'production') {
